c4ee0a91dbfccea5c7cdd9b7c172e966b7f65143
Minh-Tuan Vu <minh-tuan.vu@worldline.com>
2023-12-14 16:10:10 +0100

    14.12.2023: Refactor

--- src/main/java/com/backend/helper/MessageLoader.java
+++ src/main/java/com/backend/helper/MessageLoader.java
@@ -4,13 +4,14 @@ import com.backend.consts.RESTMessages;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.inject.Default;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 @Default
 @RequestScoped
 public class MessageLoader {
 
-    ResourceBundle bundle = ResourceBundle.getBundle("messages");
+    ResourceBundle bundle = ResourceBundle.getBundle("messages", Locale.ENGLISH);
 
     public String get(RESTMessages key){
         return bundle.getString(String.valueOf(key));
